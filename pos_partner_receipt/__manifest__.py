809d3cfc2f15145ac05ab01c255da62b311df766
Bill Morris <billingmorris@gmail.com>
2025-03-07 10:11:56 -0500

    Update __manifest__.py

--- pos_partner_receipt/__manifest__.py
+++ pos_partner_receipt/__manifest__.py
@@ -3,12 +3,12 @@
     'version': '1.0',
     'summary': 'Display partner name on POS receipt',
     'description': 'This module adds the partner name to the POS receipt.',
-    'author': 'Bill Morris',
+    'author': 'BillMorris',
     'website': 'https://billingmorris.com',
     'category': 'Point of Sale',
     'depends': ['point_of_sale'],
     'data': [],
-    'qweb': ['static/src/xml/pos_receipt.xml'],
+    'qweb': ['static/src/xml/pos_receipt_view.xml'],  # Updated file name
     'installable': True,
     'application': False,
 }
